b90ae8b2383befddf36e906f7ca1a7f0f04510c4
leo <leo.wei.badger@gmail.com>
2024-10-17 10:36:50 +0800

    img w-full

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -4,8 +4,12 @@ import { Button, Heading } from "@medusajs/ui"
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-       
-      <img src="https://medusa-2.s3.us-west-1.amazonaws.com/cv9st-8q71y.jpg"></img>
+    {/* 自适应图片 */}
+    <img 
+      src="https://medusa-2.s3.us-west-1.amazonaws.com/cv9st-8q71y.jpg" 
+      alt="Cultural Heritage" 
+      className="w-full h-full object-cover"
+    />
       {/* <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
